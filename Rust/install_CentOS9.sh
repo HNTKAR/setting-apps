9c7813a5342dd1eafbde5347dbcf7cadf701d049
kusari-k <45356209+kusari-k@users.noreply.github.com>
2022-01-25 20:13:17 +0900

    fix Rust install

--- Rust/install_CentOS9.sh
+++ Rust/install_CentOS9.sh
@@ -1,13 +1,5 @@
-sudo dnf -y install expect
 curl --proto '=https' --tlsv1.2 -o RustSetup.sh -sSf https://sh.rustup.rs
 chmod 777 RustSetup.sh
-expect -c "
-spawn ./RustSetup.sh
-expect \">\"
-send -- \"1\n\"
-interact
-exit 0
-"
+./RustSetup.sh -y
 source $HOME/.cargo/env
-# rustup component add rls rust-analysis rust-src
 rm RustSetup.sh
